94ac6860b4916f54764ffd4d5952cf6cb9e4d171
Steven R <stevenroomberg@gmail.com>
2017-03-31 19:57:51 -0400

    Text-based CGOL working

--- CGOL/CGOL.py
+++ CGOL/CGOL.py
@@ -5,9 +5,10 @@ class CGOL:
 		self.size = size
 		self.cells = []
 		for i in range(0, self.size):
-			self.cells.append([])
+			row = []
 			for j in range(0, self.size):
-				self.cells[i][j].append(Cell(j,i,0)) # init all cells to off
+				row.append(Cell(j,i,0))
+			self.cells.append(row)
 
 	def set_pieces(self):
 		print('HINT: You may only turn pieces on. The lifecycle will turn them off.')
@@ -15,14 +16,28 @@ class CGOL:
 		ran_once = False
 		while 1:
 			if ran_once:
-				cont = input('Would you like to add a piece? (y or n) ')
-				if cont.strip() == 'n':
+				try:
+					cont = int(input('Would you like to add another piece? (1 to continue, anything else to stop) '))
+				except SyntaxError:
+					print('INVALID ENTRY. TRY AGAIN.')
+					continue
+				except NameError:
+					break
+
+				if cont != 1:
 					break
 			else:
 				ran_once = True
 
-			x = int(input('X value (0-' + self.size - 1 + '): '))
-			y = int(input('Y value (0-' + self.size - 1 + '): '))
+			try:
+				x = int(input('X value (0-' + str(self.size - 1) + '): '))
+				y = int(input('Y value (0-' + str(self.size - 1) + '): '))
+			except SyntaxError:
+				print('INVALID ENTRY. TRY AGAIN.')
+				continue
+			except NameError:
+				print('INVALID ENTRY. TRY AGAIN.')
+				continue
 
 			if x >= self.size or y >= self.size:
 				print('Invalid Coordinates!')
@@ -33,7 +48,7 @@ class CGOL:
 		for i in range(0, self.size):
 			for j in range(0, self.size):
 				current_cell = self.cells[i][j]
-				num_neighbors = get_neighbors(current_cell)
+				num_neighbors = self.get_neighbors(current_cell)
 				current_state = current_cell.get_state()
 				if current_state == 0:
 					if num_neighbors == 3:
@@ -61,7 +76,7 @@ class CGOL:
 				if self.cells[y][x+1].get_state(): neighbor_count += 1
 				if self.cells[y+1][self.size-1].get_state(): neighbor_count += 1
 				if self.cells[y+1][x].get_state(): neighbor_count += 1
-				if self.cells[y+1][x+1],get_state(): neighbor_count += 1
+				if self.cells[y+1][x+1].get_state(): neighbor_count += 1
 			elif y == self.size - 1:
 				if self.cells[y-1][self.size-1].get_state(): neighbor_count += 1
 				if self.cells[y-1][x].get_state(): neighbor_count += 1
@@ -139,6 +154,14 @@ class CGOL:
 
 		return neighbor_count
 
+	def __str__(self):
+		ret = ''
+		for i in range(0, self.size):
+			for j in range(0, self.size):
+				ret += str(self.cells[i][j].get_state())
+				ret += ' '
+			ret += '\n'
+		return ret
 
 class Cell:
 
@@ -159,22 +182,38 @@ class Cell:
 	def get_y(self):
 		return self.y
 
+	def __str__(self):
+		return 'Cell: (' + str(self.get_x()) + ',' + str(self.get_y()) + ') -> ' + str(self.get_state()) + '\n'
+
 
 
 # main
+print('Program Starting')
+cgol = CGOL(20) # creates a 20x20 grid
+
 while 1:
+	print('Current Grid State:')
+	print('-----')
+	print(str(cgol))
+
+	print('\n\n')
+
 	print('Menu:')
 	print('\t1) Set pieces')
 	print('\t2) Run once')
 	print('\t3) Run multiple')
 	print('\t4) Quit')
-	user_in = input('Please select an option from the menu')
+	user_in = input('Please select an option from the menu. ')
 
 	if int(user_in) == 1:
+		cgol.set_pieces()
 
 	elif int(user_in) == 2:
+		cgol.run_once()
 
 	elif int(user_in) == 3:
+		i = input('How many iterations would you like to run the game for? ')
+		cgol.run_multiple(int(i))
 
 	elif int(user_in) == 4:
 		break
